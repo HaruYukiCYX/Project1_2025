7bd65b66fed20f12956b1d8372d60883464cc5f4
Chen Yuxuan <642962313@qq.com>
2025-04-09 02:30:46 +0100

    Button tested to work properly

--- reaction.py
+++ reaction.py
@@ -3,7 +3,18 @@ from time import sleep
 from random import uniform
 
 led = LED(4)
+right_button = Button(15)
+left_button = Button(14)
 
 led.on()
 sleep(uniform(5,10))
 led.off()
+
+def pressed(button):
+	if button.pin.number == 14:
+		print(left_name + 'won the game')
+	else:
+		print(right_name + 'won the game')
+	
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed
