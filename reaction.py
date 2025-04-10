4545d4881cc0b2b273a011613c1f8fcd8e76ac33
Chen Yuxuan <642962313@qq.com>
2025-04-10 04:23:53 +0100

    get user names

--- reaction.py
+++ reaction.py
@@ -6,6 +6,9 @@ led = LED(4)
 right_button = Button(15)
 left_button = Button(14)
 
+left_name = input('left player name is')
+right_name = input('right player name is')
+
 led.on()
 sleep(uniform(5,10))
 led.off()
